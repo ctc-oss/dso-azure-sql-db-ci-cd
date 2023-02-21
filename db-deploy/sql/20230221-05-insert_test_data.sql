461aaaa4dbbd0a7d3047f0ea6198c97ecd2266ce
hrovathj <hrovathj@users.noreply.github.com>
2023-02-21 11:25:09 -0500

    Updating data

--- db-deploy/sql/20230221-05-insert_test_data.sql
+++ db-deploy/sql/20230221-05-insert_test_data.sql
@@ -1,2 +1,2 @@
-INSERT [weather_station].[temperature_records] ([record_id], [name], [weather_device_id], [read_time], [temperaturef], [temperaturec], [humidity], [gps_device_id], [lat], [lon]) VALUES (N'57bc2fad-c2a0-4b71-8b75-fecfd601b2e0', N'mrdso-sensors', N'weather-0e10267caa23', CAST(N'2023-02-21T10:39:56.0000000' AS DateTime2), 33, 0, 25, N'gps-0e10267caa23', N'39.14756988', N'-77.750005684')
+INSERT [weather_station].[temperature_records] ([name], [weather_device_id], [read_time], [temperaturef], [temperaturec], [humidity], [gps_device_id], [lat], [lon]) VALUES (N'mrdso-sensors', N'weather-0e10267caa23', CAST(N'2023-02-21T10:39:56.0000000' AS DateTime2), 33, 0, 25, N'gps-0e10267caa23', N'39.14756988', N'-77.750005684')
 GO
